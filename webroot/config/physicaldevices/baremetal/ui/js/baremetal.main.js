502a54245829fb417a725fc11b5d466a0830a32b
Siva Bavanasi <kbsiva@juniper.net>
2014-11-10 18:35:31 +0530

    created baremetal page related files with skeleton

---
+++ webroot/config/physicaldevices/baremetal/ui/js/baremetal.main.js
@@ -0,0 +1,23 @@
+/*
+ * Copyright (c) 2014 Juniper Networks, Inc. All rights reserved.
+ */
+
+var baremetalPageLoader = new BaremetalPageLoader();
+function BaremetalPageLoader() {
+    this.load = function (paramObject) {
+        var currMenuObj = globalObj.currMenuObj,
+            rootDir = currMenuObj['resources']['resource'][0]['rootDir'],
+            pathBaremetalView = rootDir + '/js/views/BaremetalView.js',
+            hashParams = paramObject['hashParams'];
+
+        requirejs([pathBaremetalView], function (BaremetalView) {
+            var baremetalView = new BaremetalView();
+            baremetalView.render({hashParams: hashParams});
+        });
+    };
+    this.updateViewByHash = function (hashObj, lastHashObj) {
+        this.load({hashParams : hashObj});
+    };
+    this.destroy = function () {
+    };
+};
